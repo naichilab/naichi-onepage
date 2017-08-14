7d7352b29d464cef811675e1e1e9f5529b909851
naichilab <naichilab@live.jp>
2017-08-15 02:20:56 +0900

    add LinkedSocialAccount model

--- app/User.php
+++ app/User.php
@@ -9,6 +9,10 @@ class User extends Authenticatable
 {
     use Notifiable;
 
+    public function accounts(){
+      return $this->hasMany('App\LinkedSocialAccount');
+    }
+
     /**
      * The attributes that are mass assignable.
      *
